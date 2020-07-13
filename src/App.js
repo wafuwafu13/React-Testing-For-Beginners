6d5ae4d45fa32bdf7c37392cd2a4a8f6ab128c27
wafuwafu13 <mariobaske@i.softbank.jp>
2020-07-13 17:32:28 +0900

    4_Writing_Integration_Tests

--- src/App.js
+++ src/App.js
@@ -1,1 +1,5 @@
 export const add = (x, y) => x + y
+
+export const total = (shipping, subtotal) => {
+    return '$' + add(shipping, subtotal)
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
-import { add } from './App'
+import { add, total } from './App'
 
 // Unit test | It only test one thing
 test('add', () => {
     expect(add(1, 2)).toBe(3)
+})
+
+// Integration test | when add does not pass, it also does not pass 
+test('total', () => {
+    expect(total(5, 20)).toBe('$25')
 })
