a06b11d5b6865235c6690910abd5757d4be5bb01
wafuwafu13 <mariobaske@i.softbank.jp>
2020-07-13 17:52:12 +0900

    5_Mock_Functions

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,16 @@
-import { add, total } from './App'
+import { total } from './App'
+
+const add = jest.fn(() => 3)
 
 // Unit test | It only test one thing
 test('add', () => {
     expect(add(1, 2)).toBe(3)
+    expect(add(3, 6)).toBe(3)
+    expect(add).toHaveBeenCalledTimes(2)
 })
 
 // Integration test | when add does not pass, it also does not pass 
 test('total', () => {
     expect(total(5, 20)).toBe('$25')
+    expect(add).toHaveBeenCalledTimes(2)
 })
